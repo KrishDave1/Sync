bfbe9da9dfa73804b21b194638df212768fc9ebe
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-08-25 18:15:41 +0530

    Work in progess for displaying chat messages

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -7,6 +7,7 @@ import { messageArrayValidator } from "@/schemas/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 import { FC } from "react";
+import Image from "next/image";
 
 interface PageProps {
   params: {
@@ -56,7 +57,31 @@ const page: FC<PageProps> = async ({ params }: PageProps) => {
 
   const initialMessages = await getChatMessages(chatId);
 
-  return <div>{params.chatId}</div>;
+  return (
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh - 6rem)]'>
+      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+        <div className='relative flex items-center space-x-4'>
+          <div className='relative'>
+            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+              <Image
+                fill
+                referrerPolicy='no-referrer'
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className='rounded-full'
+              />
+            </div>
+          </div>
+
+
+
+
+
+          
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
