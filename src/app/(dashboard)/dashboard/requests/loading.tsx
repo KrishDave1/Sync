ebc2d28687adfbd3f1b5de91447feaa692422170
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-09-07 17:43:05 +0530

    Loading pages are added to the dashboard, requests

--- src/app/(dashboard)/dashboard/requests/loading.tsx
+++ src/app/(dashboard)/dashboard/requests/loading.tsx
@@ -16,12 +16,13 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 interface loadingProps {}
 
-const loading: FC<loadingProps> = ({ }) => {
+const loading: FC<loadingProps> = ({}) => {
   return (
     <div className='w-full flex flex-col gap-3 mt-8'>
       <Skeleton className='mb-4' height={60} width={500} />
-      <Skeleton height={20} width={150} />
-      <Skeleton height={50} width={400} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
+      <Skeleton height={50} width={350} />
     </div>
   );
 };
