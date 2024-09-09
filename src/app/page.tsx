aa08f21cb9a1c1b5299c99b158f82c6d101e9838
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-09-09 17:21:36 +0530

    SEO changes made - metadata and favicon updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 /** @format */
 
 import { db } from "@/lib/db";
+import Head from "next/head";
 import Image from "next/image";
 
 export default async function Home() {
-  await db.set("hello", "world");
   return (
     <div>
       <h1>Home</h1>
-      <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+      <Image src='/favicon.ico' alt='Vercel Logo' width={72} height={16} />
     </div>
   );
 }
